4125be17e5ce55b662e7d0f94e46889cd61707ff
Shreya Baboota <babootashreya@gmail.com>
2026-03-11 18:44:10 +0530

    fix startup table creation

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from app.extentions import db
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-    print("Tables ensured")
+def ensure_tables():
+    with app.app_context():
+        db.create_all()
+        print("Tables ensured")
+
+ensure_tables()
