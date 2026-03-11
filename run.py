aeabb0dfb9942ab8c022c03dc1ebc013456ef24c
Shreya Baboota <babootashreya@gmail.com>
2026-03-11 23:24:29 +0530

    create tables

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from app.judge.worker import run_worker
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 def start_worker():
     thread = threading.Thread(target=run_worker)
     thread.daemon = True
