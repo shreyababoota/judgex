a2cb24c806876c919ef45a3ac802da738bd9524e
Shreya Baboota <babootashreya@gmail.com>
2026-03-12 10:49:01 +0530

    fix worker thread for render

--- run.py
+++ run.py
@@ -1,18 +1,27 @@
 from app import create_app
 from app.extentions import db
-import threading
 from app.judge.worker import run_worker
+import threading
 
 app = create_app()
 
+# create tables if they don't exist
 with app.app_context():
     db.create_all()
 
+
 def start_worker():
-    thread = threading.Thread(target=run_worker)
+    def worker():
+        with app.app_context():
+            print("Judge worker started")
+            run_worker()
+
+    thread = threading.Thread(target=worker)
     thread.daemon = True
     thread.start()
 
+start_worker()
+
+
 if __name__ == "__main__":
-    start_worker()
     app.run(debug=True)
